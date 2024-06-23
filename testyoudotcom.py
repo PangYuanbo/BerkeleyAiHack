b138ab15864c1456645b44d39756ca6b4f8f4a21
PangYuanbo <yuanbopang@outlook.com>
2024-06-22 18:12:23 -0700

    init

--- testyoudotcom.py
+++ testyoudotcom.py
@@ -3,7 +3,7 @@ import requests
 url = "https://chat-api.you.com/research"
 
 payload = {
-    "query": "Where is the restraut located in UCB for ",
+    "query": "Where is the restraut located in UCB for lunch with google map link?",
     "chat_id": "3c90c3cc-0d44-4b50-8888-8dd25736052a"
 }
 headers = {
